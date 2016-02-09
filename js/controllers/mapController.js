867411bdc31fdf3dec8f74e928ef0e2e547b49ed
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-02-08 18:01:15 -0900

    Map controller now uses image overlays with pre-determined boundaries in preparation for a fully offline mode.

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -3,44 +3,62 @@ angular.module('starter').controller('MapController',
     '$cordovaGeolocation',
     '$stateParams',
     '$ionicModal',
-    '$ionicPopup',
     'LocationsService',
-    'InstructionsService',
     function(
       $scope,
       $cordovaGeolocation,
       $stateParams,
       $ionicModal,
-      $ionicPopup,
-      LocationsService,
-      InstructionsService
+      LocationsService
       ) {
 
-      /**
-       * Once state loaded, get put map on scope.
-       */
       $scope.$on("$stateChangeSuccess", function() {
 
-        $scope.locations = LocationsService.savedLocations;
-        $scope.newLocation;
+        proj4.defs([
+          [ 'EPSG:3338', '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs' ]
+        ]);
 
-        if(!InstructionsService.instructions.newLocations.seen) {
+        // The EPSG:3338 boundary values used to fetch raw PNG images from
+        // various WMS servers. These boundary values are a (nearly) perfect
+        // square. They were also borrowed from Leaflet's WMS tile boundaries.
+        // You can set these values to anything that forms a perfect square,
+        // but the image seems to be sharper when it aligns to the values
+        // Leaflet chooses.
+        var epsg3338_ll = [-116210.93749999953, 2354980.4687499963];
+        var epsg3338_ur = [-88867.1874999995, 2382324.2187499986];
 
-          var instructionsPopup = $ionicPopup.alert({
-            title: 'Add Locations',
-            template: InstructionsService.instructions.newLocations.text
-          });
-          instructionsPopup.then(function(res) {
-            InstructionsService.instructions.newLocations.seen = true;
-            });
-
-        }
+        // Convert the EPSG:3338 boundaries to EPSG:4326 lat/lon values to set
+        // maxbounds and layer bounds.
+        var epsg4326_ll = proj4('EPSG:3338', 'EPSG:4326', epsg3338_ll);
+        var epsg4326_ur = proj4('EPSG:3338', 'EPSG:4326', epsg3338_ur);
 
         $scope.map = {
           defaults: {
-            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
+            crs: new L.Proj.CRS('EPSG:3338', proj4.defs('EPSG:3338'), 
+              {
+                bounds: L.bounds([-3500000,-3500000], [3500000,3500000]),
+                origin: [-3500000,3500000],
+                resolutions: [27343.75, 13671.875, 6835.9375, 3417.96875, 1708.984375, 854.4921875, 427.24609375, 213.623046875, 106.8115234375, 53.40576171875, 26.702880859375, 13.3514404296875, 6.67572021484375, 3.337860107421875, 1.6689300537109375, 0.8344650268554688, 0.4172325134277344, 0.2086162567138672, 0.1043081283569336, 0.0521540641784668, 0.0260770320892334]
+              }
+            ),
+            minZoom: 11,
+            maxZoom: 14,
             zoomControlPosition: 'bottomleft'
+          },
+          center: {
+            lat: 71.295556,
+            lng: -156.766389,
+            zoom: 11
+          },
+          maxbounds: {
+            southWest: {
+              lat: epsg4326_ll[1],
+              lng: epsg4326_ll[0]
+            },
+            northEast: {
+              lat: epsg4326_ur[1],
+              lng: epsg4326_ur[0]
+            }
           },
           markers : {},
           events: {
@@ -48,11 +66,42 @@ angular.module('starter').controller('MapController',
               enable: ['context'],
               logic: 'emit'
             }
+          },
+          layers: {
+            baselayers: {
+              bdl: {
+                name: 'BDL',
+                type: 'imageOverlay',
+                url: 'http://wms.alaskamapped.org/bdl?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=BestDataAvailableLayer&tiled=true&WIDTH=3000&HEIGHT=3000&CRS=EPSG%3A3338&STYLES=&BBOX=' + epsg3338_ll[0] + ',' + epsg3338_ll[1] + ',' + epsg3338_ur[0] + ',' + epsg3338_ur[1],
+                bounds: [[epsg4326_ll[1], epsg4326_ll[0]], [epsg4326_ur[1], epsg4326_ur[0]]],
+                layerParams: {
+                  showOnSelector: false,
+                  continuousWorld: true
+                }
+              },
+            },
+            overlays: {
+              barrow: {
+                name: 'Barrow',
+                type: 'imageOverlay',
+                url: 'http://geonode-test.iarc.uaf.edu/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=geonode%3Abarrow_radar_3338_transparent_really&tiled=true&WIDTH=1023&HEIGHT=1023&CRS=EPSG%3A3338&STYLES=&BBOX=' + epsg3338_ll[0] + ',' + epsg3338_ll[1] + ',' + epsg3338_ur[0] + ',' + epsg3338_ur[1],
+                bounds: [[epsg4326_ll[1], epsg4326_ll[0]], [epsg4326_ur[1], epsg4326_ur[0]]],
+                layerParams: {
+                  showOnSelector: true,
+                  continuousWorld: true
+                }
+              }
+            }
           }
         };
 
-        $scope.goTo(0);
-
+        $scope.map.markers['barrow'] = {
+          lat: $scope.map.center.lat,
+          lng: $scope.map.center.lng,
+          message: "Barrow, Alaska",
+          focus: true,
+          draggable: false
+        };
       });
 
       var Location = function() {
@@ -62,64 +111,14 @@ angular.module('starter').controller('MapController',
         this.name = "";
       };
 
-      $ionicModal.fromTemplateUrl('templates/addLocation.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-          $scope.modal = modal;
-        });
-
-      /**
-       * Detect user long-pressing on map to add new location
-       */
-      $scope.$on('leafletDirectiveMap.contextmenu', function(event, locationEvent){
-        $scope.newLocation = new Location();
-        $scope.newLocation.lat = locationEvent.leafletEvent.latlng.lat;
-        $scope.newLocation.lng = locationEvent.leafletEvent.latlng.lng;
-        $scope.modal.show();
-      });
-
-      $scope.saveLocation = function() {
-        LocationsService.savedLocations.push($scope.newLocation);
-        $scope.modal.hide();
-        $scope.goTo(LocationsService.savedLocations.length - 1);
-      };
-
-      /**
-       * Center map on specific saved location
-       * @param locationKey
-       */
-      $scope.goTo = function(locationKey) {
-
-        var location = LocationsService.savedLocations[locationKey];
-
-        $scope.map.center  = {
-          lat : location.lat,
-          lng : location.lng,
-          zoom : 12
-        };
-
-        $scope.map.markers[locationKey] = {
-          lat:location.lat,
-          lng:location.lng,
-          message: location.name,
-          focus: true,
-          draggable: false
-        };
-
-      };
-
-      /**
-       * Center map on user's current position
-       */
+      // Place GPS coordinate on map.
       $scope.locate = function(){
-
         $cordovaGeolocation
           .getCurrentPosition()
           .then(function (position) {
             $scope.map.center.lat  = position.coords.latitude;
             $scope.map.center.lng = position.coords.longitude;
-            $scope.map.center.zoom = 15;
+            $scope.map.center.zoom = 11;
 
             $scope.map.markers.now = {
               lat:position.coords.latitude,
@@ -128,13 +127,10 @@ angular.module('starter').controller('MapController',
               focus: true,
               draggable: false
             };
-
           }, function(err) {
             // error
             console.log("Location error!");
             console.log(err);
           });
-
       };
-
     }]);
