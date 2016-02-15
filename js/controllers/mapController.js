961e51a3242e3e9f7312c55ce96542de120ac92b
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-02-15 13:24:46 -0900

    Leaflet support for image overlays in alternate projections is limited. Using default EPSG:3857 projection for now.

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -13,41 +13,20 @@ angular.module('starter').controller('MapController',
       ) {
 
       $scope.$on("$stateChangeSuccess", function() {
-
-        proj4.defs([
-          [ 'EPSG:3338', '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs' ]
-        ]);
-
-        // The EPSG:3338 boundary values used to fetch raw PNG images from
-        // various WMS servers. These boundary values are a (nearly) perfect
-        // square. They were also borrowed from Leaflet's WMS tile boundaries.
-        // You can set these values to anything that forms a perfect square,
-        // but the image seems to be sharper when it aligns to the values
-        // Leaflet chooses.
-        var epsg3338_ll = [-116210.93749999953, 2354980.4687499963];
-        var epsg3338_ur = [-88867.1874999995, 2382324.2187499986];
-
-        // Convert the EPSG:3338 boundaries to EPSG:4326 lat/lon values to set
-        // maxbounds and layer bounds.
-        var epsg4326_ll = proj4('EPSG:3338', 'EPSG:4326', epsg3338_ll);
-        var epsg4326_ur = proj4('EPSG:3338', 'EPSG:4326', epsg3338_ur);
+        var epsg4326_ll = [-157.237324185, 71.1479777401];
+        var epsg4326_ur = [-156.339008901, 71.4361019042];
+        var epsg3857_ll = proj4('EPSG:3857', epsg4326_ll);
+        var epsg3857_ur = proj4('EPSG:3857', epsg4326_ur);
 
         $scope.map = {
           defaults: {
-            crs: new L.Proj.CRS('EPSG:3338', proj4.defs('EPSG:3338'), 
-              {
-                bounds: L.bounds([-3500000,-3500000], [3500000,3500000]),
-                origin: [-3500000,3500000],
-                resolutions: [27343.75, 13671.875, 6835.9375, 3417.96875, 1708.984375, 854.4921875, 427.24609375, 213.623046875, 106.8115234375, 53.40576171875, 26.702880859375, 13.3514404296875, 6.67572021484375, 3.337860107421875, 1.6689300537109375, 0.8344650268554688, 0.4172325134277344, 0.2086162567138672, 0.1043081283569336, 0.0521540641784668, 0.0260770320892334]
-              }
-            ),
-            minZoom: 8,
-            maxZoom: 13,
+            minZoom: 9,
+            maxZoom: 14,
             zoomControlPosition: 'bottomleft'
           },
           center: {
-            lat: 71.295556,
-            lng: -156.766389,
+            lat: 71.292574658,
+            lng: -156.788166543,
             zoom: 11
           },
           maxbounds: {
@@ -72,7 +51,7 @@ angular.module('starter').controller('MapController',
               bdl: {
                 name: 'BDL',
                 type: 'imageOverlay',
-                url: 'http://wms.alaskamapped.org/bdl?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=BestDataAvailableLayer&tiled=true&WIDTH=3000&HEIGHT=3000&CRS=EPSG%3A3338&STYLES=&BBOX=' + epsg3338_ll[0] + ',' + epsg3338_ll[1] + ',' + epsg3338_ur[0] + ',' + epsg3338_ur[1],
+                url: 'http://wms.alaskamapped.org/bdl?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=BestDataAvailableLayer&tiled=true&WIDTH=3000&HEIGHT=3000&CRS=EPSG%3A3857&STYLES=&BBOX=' + epsg3857_ll[0] + ',' + epsg3857_ll[1] + ',' + epsg3857_ur[0] + ',' + epsg3857_ur[1],
                 bounds: [[epsg4326_ll[1], epsg4326_ll[0]], [epsg4326_ur[1], epsg4326_ur[0]]],
                 layerParams: {
                   showOnSelector: false,
@@ -84,7 +63,7 @@ angular.module('starter').controller('MapController',
               barrow: {
                 name: 'Barrow',
                 type: 'imageOverlay',
-                url: 'http://geonode-test.iarc.uaf.edu/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=geonode%3Abarrow_radar_3338_transparent_really&tiled=true&WIDTH=1023&HEIGHT=1023&CRS=EPSG%3A3338&STYLES=&BBOX=' + epsg3338_ll[0] + ',' + epsg3338_ll[1] + ',' + epsg3338_ur[0] + ',' + epsg3338_ur[1],
+                url: 'http://geonode-test.iarc.uaf.edu/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=geonode%3Abarrow_epsg3857&tiled=true&WIDTH=1100&HEIGHT=1100&CRS=EPSG%3A3857&STYLES=&BBOX=' + epsg3857_ll[0] + ',' + epsg3857_ll[1] + ',' + epsg3857_ur[0] + ',' + epsg3857_ur[1],
                 bounds: [[epsg4326_ll[1], epsg4326_ll[0]], [epsg4326_ur[1], epsg4326_ur[0]]],
                 layerParams: {
                   showOnSelector: true,
@@ -96,8 +75,8 @@ angular.module('starter').controller('MapController',
         };
 
         $scope.map.markers['barrow'] = {
-          lat: $scope.map.center.lat,
-          lng: $scope.map.center.lng,
+          lat: 71.295556,
+          lng: -156.766389,
           message: "Barrow, Alaska",
           focus: true,
           draggable: false
